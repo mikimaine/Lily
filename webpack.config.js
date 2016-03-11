71f895aa7de9f8881f7205d3e2ff844403920cb5
Moe Szyslak <moe.duffdude@gmail.com>
2016-03-11 16:27:43 +0300

    webpack configuration
    react 15.0.0 🙌🏿
    
    taking the plunge son!

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
 
 module.exports = {
   entry: ['./app/index.js'],
@@ -10,12 +11,36 @@ module.exports = {
     loaders: [
       { test: /\.js$/, include: __dirname + '/app', loader: 'babel-loader' }
     ]
-  },
-  plugins: [
+  }
+};
+
+// depending on the arugments passes we'll be adding specific
+// plugins to webpack else we'll have production build even on webpack dev server
+if (process.argv.indexOf('-p') > -1) {
+  module.exports.devtool = '#source-map';
+  module.exports.plugins = [
+    new HtmlWebpackPlugin({
+      template: __dirname + '/app/index.html',
+      filename: 'index.html',
+      inject: 'body'
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ];
+} else {
+  module.exports.plugins = [
     new HtmlWebpackPlugin({
       template: __dirname + '/app/index.html',
       filename: 'index.html',
       inject: 'body'
     })
-  ]
-};
+  ];
+}
